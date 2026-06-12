2cc6e1636c8515c1bd2c08024a4349d3d0126056
asyongasyong123 <asyongasyong123@gmail.com>
2026-06-12 17:04:39 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 set -e
 
-PORT=${PORT:-8080}
-echo "Using PORT=$PORT"
-
-xray test -c /etc/xray.json
-
-sed -i "s/listen 8080;/listen ${PORT};/g" /usr/local/openresty/nginx/conf/nginx.conf
-sed -i "s/listen \[::\]:8080;/listen [::]:${PORT};/g" /usr/local/openresty/nginx/conf/nginx.conf
-
 xray run -c /etc/xray.json &
-
-exec openresty -g 'daemon off;'
+exec /usr/local/openresty/bin/openresty -g "daemon off;"
