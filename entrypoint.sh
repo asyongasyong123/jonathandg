44d3f0f9fd7d0d62b94002d8f73ace684596a669
asyongasyong123 <asyongasyong123@gmail.com>
2026-06-12 19:00:33 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 set -e
 
-# Gamita ang port nga gihatag sa Cloud Run, kung wala 8080 ang gamiton
-PORT=${PORT:-8080}
+# Kuhaa ang port gikan sa Cloud Run, default 8080 kung wala
+PORT="${PORT:-8080}"
+echo "Using port: $PORT"
 
-# Ilisan ang port sa nginx config aron motugma sa $PORT
+# Ilisan ang port sa nginx.conf
 sed -i "s|listen 8080;|listen ${PORT};|g" /usr/local/openresty/nginx/conf/nginx.conf
+
+# I-check kung nausab na
+echo "Nginx config updated to listen on port $PORT"
 
 # Sugdi ang Xray ug OpenResty
 xray run -c /etc/xray.json &
